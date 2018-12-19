eae4228df0ca8d9b9c9bdbed08ce2dc0dfafb7aa
AlexAndriamahaleo <alex.andriamahaleo@etu.univ-amu.fr>
2018-12-19 15:47:23 +0100

    Unit Test with Karma [4/6]

--- src/app/chat/services/chat.service.spec.ts
+++ src/app/chat/services/chat.service.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import { ChatService } from './chat.service';
+import {HttpClient} from '@angular/common/http';
+import {Message} from '../models/message';
 
 describe('ChatService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ChatService, HttpClient, Message]
+  }));
 
-  it('should be created', () => {
-    const service: ChatService = TestBed.get(ChatService);
+  it('should ...', inject([ChatService], (service: ChatService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
